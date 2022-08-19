7c79d8ba4f2bdcb613013462275cc3d9fbbfdfaf
Geet George <geet.george@mpimet.mpg.de>
2022-08-19 22:40:16 +0200

    add reg_line function

--- src/plotting_functions.py
+++ src/plotting_functions.py
@@ -1,4 +1,38 @@
 import numpy as np
+import scipy as sp
+
+
+def reg_line(
+    ax,
+    x,
+    y,
+    print_r=True,
+    text_title=True,
+    r_loc=(0.75, 0.75),
+    kwargs_for_text={},
+    kwargs_for_axline={},
+):
+    """Plots regression line"""
+
+    m, b = np.polyfit(x, y, 1)
+
+    if print_r:
+        r, _ = sp.stats.pearsonr(x, y)
+
+        if text_title:
+            ax.set_title("r={:.2f}".format(r), **kwargs_for_text)
+        else:
+            ax.text(
+                r_loc[0],
+                r_loc[1],
+                "r={:.2f}".format(r),
+                transform=ax.transAxes,
+                **kwargs_for_text
+            )
+
+    plt.axline(xy1=(0, b), slope=m, **kwargs_for_axline)
+
+    return ax
 
 
 def sci_exp_axis(i, axis="x", x_pad=0.75, y_pad=0.01, scilimits=(-1, 1)):
